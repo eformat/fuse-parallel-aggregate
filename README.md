9ec97f1d319c04a56ea2d861e50d4e43faeaa562
Mike Hepburn <eformat@gmail.com>
2019-09-13 12:59:56 +1000

    code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fuse-parallel-aggregate
+
+```
+mvn package -DskipTests
+java -jar target/fuse-parallel-aggregate-1.0-SNAPSHOT-runner.jar
+```
---
+++ src/main/java/org/acme/quickstart/CamelRoute.java
@@ -0,0 +1,57 @@
+package org.acme.quickstart;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ThreadPoolBuilder;
+import org.apache.camel.util.concurrent.ThreadPoolRejectedPolicy;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class CamelRoute extends RouteBuilder {
+
+  @Override
+    public void configure() throws Exception {
+      errorHandler(defaultErrorHandler().redeliveryDelay(3000).maximumRedeliveries(1));
+
+      ThreadPoolBuilder builder = new ThreadPoolBuilder(getContext());
+      ExecutorService pool = builder.poolSize(10).maxPoolSize(30).keepAliveTime(Long.valueOf("5"), TimeUnit.MINUTES)
+          .rejectedPolicy(ThreadPoolRejectedPolicy.CallerRuns).build("xxx");
+  
+      from("timer://trigger?period=200").
+              process(exchange -> {
+                  exchange.getOut().setBody(Arrays.asList(1, 2, 3, 4, 2, 5, 6, 2, 10, 11));
+              }).
+              split(body(), new SimpleAggregator())              
+              .parallelAggregate()
+              .executorService(pool)
+              .process(exchange1 -> {
+                  System.out.println(Thread.currentThread() + " - STARTING" + exchange1.getIn().getBody());                  
+                  if (exchange1.getIn().getBody(Integer.class) == 2) {
+                      //System.out.println(Thread.currentThread() + " - Headers: " + exchange1.getIn().getHeaders());
+                      if (exchange1.getIn().getHeader("CamelRedelivered", Boolean.class) == Boolean.TRUE) {
+                          return;
+                      }
+                      throw new RuntimeException("Error body 2");
+                  }
+                  Thread.sleep((long) (Math.random() * 8000));
+                  System.out.println(Thread.currentThread() + " - ENDING" + exchange1.getIn().getBody() + ;
+              })
+              .end()
+              .to("log:out");
+  }
+
+  class SimpleAggregator implements AggregationStrategy {
+
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+      // !! Should be thread safe !!
+      System.out.println(Thread.currentThread() + " - aggregate ! total threads: " + java.lang.Thread.activeCount());
+      return newExchange;
+    }
+  }
+
+}
